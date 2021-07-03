adec06cbf183b83f8f750b929aa8481ea6bb1311
Siddharth Oza <55457720+SiddharthOza00@users.noreply.github.com>
2021-07-03 11:14:29 +0530

    Update main.py

--- Code/main.py
+++ Code/main.py
@@ -9,16 +9,13 @@ screen.addshape(image)
 turtle.shape(image)
 
 guessed_states = []
-missed_states = []
 
 data = pandas.read_csv("50_states.csv")
 all_states = data.state.to_list()
 while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50", prompt="Enter the state's name?").title()
     if answer_state == "Exit":
-        for states in all_states:
-            if states not in guessed_states:
-                missed_states.append(states)
+        missed_states = [state for state in all_states if state not in guessed_states]
         new = pandas.DataFrame(missed_states)
         missed = new.to_csv("states_to_learn.csv")
         break
